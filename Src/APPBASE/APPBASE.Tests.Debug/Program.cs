88fae5c0896fc1405d81defa948f2fe781784caf
sugaprivate <arinsuga@gmail.com>
2020-03-20 09:23:21 -0700

    master - update .NET Framework dari .NET 4.x.x jadi .NET Core 2.1

---
+++ Src/APPBASE/APPBASE.Tests.Debug/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using APPBASE.Tests;
+
+namespace APPBASE.Tests.Debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorTest oTest = new CalculatorTest();
+            oTest.Calculator_getGrossAmount_2Jt();
+        }
+    }
+}
